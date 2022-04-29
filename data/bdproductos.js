9a3509e9163f03efafa4d7252beb30a1a2fbc9c2
Alexandra Sivira <kerensivira@gmail.com>
2022-04-28 22:27:44 -0500

    ...

---
+++ data/bdproductos.js
@@ -0,0 +1,148 @@
+{
+    "ofertas" [
+        {
+            "id": 1,
+            "nombre": "Limon con semilla",
+            "precioFinal": 26.8,
+            "precioInicial": 39.9,
+            "imagen": "https://res.cloudinary.com/alexa01020304/image/upload/v1651198481/limonSemillas_uejfc3.jpg"
+        },
+        {
+            "id": 2,
+            "nombre": "Tomate",
+            "precioFinal": 21.7,
+            "precioInicial": 32.9,
+            "imagen": "https://res.cloudinary.com/alexa01020304/image/upload/v1647474401/tomoto_ujhvql.jpg"
+        },
+        {
+            "id": 3,
+            "nombre": "Aguacate Hass",
+            "precioFinal": 56.8,
+            "precioInicial": 69.9,
+            "imagen": "https://res.cloudinary.com/alexa01020304/image/upload/v1647474400/aguacate_kougss.jpg"
+        },
+        {
+            "id": 4,
+            "nombre": "Nopal Limpio",
+            "precioFinal": 25.8,
+            "precioInicial": 29.9,
+            "imagen": "https://res.cloudinary.com/alexa01020304/image/upload/v1651200677/nopal_dzxysb.png"
+        },
+        {
+            "id": 5,
+            "nombre": "Tomate verde sin cascara",
+            "precioFinal": 22.8,
+            "precioInicial": 24.9,
+            "imagen": "https://res.cloudinary.com/alexa01020304/image/upload/v1651200788/tomateVerde_l70wzn.jpg"
+        },
+        {
+            "id": 6,
+            "nombre": "Coca-Cola Sin Azucar",
+            "precioFinal": 26,
+            "precioInicial": 28,
+            "imagen": "https://res.cloudinary.com/alexa01020304/image/upload/v1651200905/cocasinazucar_eguuyh.jpg"
+
+        },
+        {
+            "id": 7,
+            "nombre": "Coca-Cola",
+            "precioFinal": 12.9,
+            "precioInicial": 14,
+            "imagen": "https://res.cloudinary.com/alexa01020304/image/upload/v1651200897/cocaoriginal_wz7ua4.jpg"
+
+        }
+    ],
+    "populares" [
+        {
+            "id": 1,
+            "nombre": "San Rafael pechuga de pavo",
+            "precio": 62.5,
+            "imagen": "https://res.cloudinary.com/lau2401/image/upload/v1641500090/Name_Pechuga_de_pavo_ao7sb8.jpg"
+        },
+        {
+            "id": 2,
+            "nombre": "Pan Bimbo grande",
+            "precio": 38.9,
+            "imagen": "https://res.cloudinary.com/lau2401/image/upload/v1641500087/Name_Pan_Bimbo_gzriwe.jpg"
+        },
+        {
+            "id": 3,
+            "nombre": "Mayonesa",
+            "precio": 48.5,
+            "imagen": "https://res.cloudinary.com/lau2401/image/upload/v1641500086/Name_Mc_Cormick_Mayonesa_vxr4lc.jpg"
+        },
+        {
+            "id": 4,
+            "nombre": "Pasta dental Colgate",
+            "precio": 35.5,
+            "imagen": "https://res.cloudinary.com/lau2401/image/upload/v1641500086/Name_Colgate_ju2onf.jpg"
+        },
+        {
+            "id": 5,
+            "nombre": "Huevos san juan",
+            "precio": 37.5,
+            "imagen": "https://res.cloudinary.com/lau2401/image/upload/v1641500086/Name_Huevos_rpclev.jpg"
+        }
+    ],
+    "ubicaciones" [
+        {
+            "id": 1,
+            "ciudad": "Mosquera"
+        },
+        {
+            "id": 2,
+            "ciudad": "Fontibon"
+        },
+        {
+            "id": 3,
+            "ciudad": "Bogotá"
+        },
+        {
+            "id": 4,
+            "ciudad": "Chapinero"
+        },
+        {
+            "id": 5,
+            "ciudad": "Bosa"
+        },
+        {
+            "id": 6,
+            "ciudad": "San Cristobal"
+        },
+        {
+            "id": 7,
+            "ciudad": "Usme"
+        },
+        {
+            "id": 8,
+            "ciudad": "Tunjuelito"
+        },
+        {
+            "id": 9,
+            "ciudad": "Kennedy"
+        },
+        {
+            "id": 10,
+            "ciudad": "Engativá"
+        },
+        {
+            "id": 11,
+            "ciudad": "Suba"
+        }
+    ],
+    "usuarios" [
+        {
+            "id": 1,
+            "nombre": "Alexa",
+            "apellido": "Sivira",
+            "correo": "kerensivira@gmail.com"
+        },
+        {
+            "nombre": "Elsy",
+            "apellido": "Cordero",
+            "correo": "elsy.cordero1@gmail.com",
+            "id": 2
+        }
+    ]
+
+}
